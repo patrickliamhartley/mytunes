0bbaa7a0dabcd968872d3d5f9cfb0c6b4c34fdbb
patrickliamhartley <patrickliamhartley@gmail.com>
2016-08-10 19:57:13 -0700

    Got the SongModel tests passing for now

--- client/views/LibraryView.js
+++ client/views/LibraryView.js
@@ -1,4 +1,4 @@
-// LibraryView.js - Defines a backbone view class for the music library.
+// // LibraryView.js - Defines a backbone view class for the music library.
 var LibraryView = Backbone.View.extend({
 
   tagName: 'table',
