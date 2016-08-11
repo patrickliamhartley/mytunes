a3cc2706b7eef21da193575d6aad3c0287b388c3
patrickliamhartley <patrickliamhartley@gmail.com>
2016-08-10 21:00:27 -0700

    Start implementation of sonqueue functionality

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,25 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('change', function() {
+      console.log('yo');
+      if (this.models.length === 1) {
+        console.log('hi');
+        this.playFirst;
+      }
+    }, this);
+  
+    this.on('change', this.playFirst);
+  },
+
+  playFirst: function() {
+
+  
+    this.at(0).play();
+    
+      //song.play()
+   
+
   }
 
 });
